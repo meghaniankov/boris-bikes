bc7ce5e52a607884f98df688fe32b4ced4d2ca19
Meghan <meghan.e.mccue@gmail.com>
2019-12-10 18:04:57 +0000

    Add dock_bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,25 @@
 require_relative 'bike'
 
 class DockingStation
+    attr_reader :bike
+
+    def initalize(bikes)
+        @bike = bike
+    end
+
 
   def release_bike
-    Bike.new
+    @bike = Bike.new
+    # released_bikes = []
+    # released_bikes << release_bike
+    # puts "Released Bikes: #{released_bikes}. We have #{released_bikes.count} beautiful bikes!"
   end
 
+    def dock_bike
+        docked_bikes = []
+        docked_bikes << @bike
+        docked_bikes
+        # puts "Docked Bikes: #{docked_bikes}. We have #{docked_bikes.count} beautiful bikes docked!"
+    end
 
 end
