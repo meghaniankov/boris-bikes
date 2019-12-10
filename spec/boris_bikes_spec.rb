e22bd6a91eda1112805ca658ba35418eed52a3e4
Imraan Ali <imraan91@hotmail.co.uk>
2019-12-10 17:10:22 +0000

    updated tests

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -3,6 +3,11 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    # expect(bike).to be_working is the following code in next line
+    expect(bike.working?).to eq true
+  end
+
 
 end
-
