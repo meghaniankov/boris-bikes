9c6562f70c83302916ae4f3ee2868a890f6370d6
Meghan <meghan.e.mccue@gmail.com>
2019-12-11 14:45:29 +0000

    Challenge 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,8 +2,19 @@ require_relative 'bike'
 
 class DockingStation
 
+  def initialize
+    @bike_rack = []
+  end
+
+  attr_reader :bike_rack
+
   def release_bike
    Bike.new
   end
+
+  def dock_bike(bike)
+    @bike_rack << bike
+  end
+
 
 end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -9,5 +9,25 @@ describe DockingStation do
     expect(bike.working?).to eq true
   end
 
+  it { is_expected.to respond_to(:dock_bike).with(1).argument }
+
+  it 'accepts docked bikes' do
+    bike = subject.release_bike
+    docked_bike = subject.dock_bike(bike)
+    expect(docked_bike.empty?).to eq false
+  end
+  
+#   it 'checks how many bikes are available' do
+#     bike = subject.release_bike
+#     docked_bike = subject.dock_bike(bike)
+#     expect(subject.check_bikes_available).to eq "There are 1 bike(s) available"
+#   end
+
+it 'checks how many bikes are available' do  
+    bike = subject.release_bike
+    docked_bike = subject.dock_bike(bike)
+    expect(subject.bike_rack).to eq docked_bike.to_a
+  end
+
 
 end
